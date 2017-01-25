50e643a44b7b9b872cf144bb31cfcc413f810add
Takeshi Mizumoto <ta.mizumoto@gmail.com>
2017-01-25 19:36:15 +0900

    pypi

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # wavshow
 visualizing tool for wavefile
+
+Once you installed this package, you can use
+
+wavshow [YOUR WAV FILE]
+
+to show waveform and spectrogram.
--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
   name='wavshow',
   version='0.1',
   license='MIT',
+  author="mzmttks",
+  author_email="ta.mizumoto@gmail.com",
+  url="https://github.com/mzmttks/wavshow",
   packages=find_packages(),
   entry_points = {
     "console_scripts":{
